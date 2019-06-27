2353afb020f1764a5c770f7930a784befd4fb693
Oceane97440 <sautronoceane98@gmail.com>
2019-06-27 16:53:01 +0400

    Jwplayer preroll a faire

---
+++ JWplayer/racineJw.js
@@ -0,0 +1,6 @@
+<div itemscope itemtype="https://schema.org/VideoObject">
+<meta itemprop="uploadDate" content="Thu Jun 27 2019 10:13:13 GMT+0400 (heure du Golfe)"/>
+<meta itemprop="name" content="V2"/><meta itemprop="duration" content="PT12S" /><meta itemprop="thumbnailUrl" content="https://content.jwplatform.com/thumbs/4OBrEx6N-480.jpg"/>
+<meta itemprop="contentUrl" content="https://content.jwplatform.com/videos/4OBrEx6N-f3Gtk4Us.mp4"/>
+<script src="https://cdn.jwplayer.com/players/4OBrEx6N-ctwMqhdJ.js"></script>
+</div>
